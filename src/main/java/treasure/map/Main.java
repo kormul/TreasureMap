90ee68d94dde65c54d656fb0f9d403d87e937a14
Aubert Guillaume <kormul@gmx.fr>
2024-05-28 17:15:44 +0200

    Add Log Exec

--- src/main/java/treasure/map/Main.java
+++ src/main/java/treasure/map/Main.java
@@ -7,12 +7,19 @@ public class Main {
 	public static void main(String[] args) {
 		
 		try {
-			
+			System.out.println("Chargement du Fichier");
 			List<String> file = ReadFile.instruction("Entree.txt");
+			System.out.println("Initialisation du Jeu");
 			Game.initGame(file);
+			System.out.println("Démarrage du Jeu");
 			Game.game.play();
+			System.out.println("Fin du Jeu, génération du résultat");
 			String res = Game.resultat();
+			System.out.println("Resultat : "+ res);
+			System.out.println("Sauvegarde du résultat");
 			SaveFile.saveFile(res);
+			System.out.println("Fin");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
